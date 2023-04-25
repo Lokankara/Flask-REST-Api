ede9c48a09489baeb027a7a33d74f50f793d9445
Pasha <78595276+Lokankara@users.noreply.github.com>
2023-04-25 20:17:26 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,33 +1,24 @@
-import json
 import unittest
+from flask import Flask
 from app import app
 
-class ParaphraseTestCase(unittest.TestCase):
+class TestParaphraseEndpoint(unittest.TestCase):
 
     def setUp(self):
-        app.testing = True
-        self.client = app.test_client()
+        self.app = app.test_client()
 
-    def test_missing_tree_parameter(self):
-        response = self.client.get('/paraphrase')
-        data = json.loads(response.data.decode())
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(data['error'], 'Missing "tree" parameter.')
-
-    def test_invalid_tree_string(self):
-        response = self.client.get('/paraphrase?tree=invalid')
-        data = json.loads(response.data.decode())
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(data['error'], 'Invalid tree string.')
-
-    def test_generate_paraphrases(self):
-        tree_str = "(S (NP (DT The) (NN cat)) (VP (VBD sat) (PP (IN on) (NP (DT the) (NN mat)))))"
-        response = self.client.get(f'/paraphrase?tree={tree_str}&limit=2')
-        data = json.loads(response.data.decode())
+    def test_paraphrase_endpoint_success(self):
+        tree_str = "(S (NP (DT The) (NN cat)) (VP (VBD sat) (PP (IN on) (NP (DT a) (NN mat)))))"
+        expected = {"paraphrases": [{"tree": "(S (NP (DT a) (NN mat)) (VP (VBD sat) (PP (IN on) (NP (DT The) (NN cat)))))"}]}
+        response = self.app.get(f"/paraphrase?tree={tree_str}&limit=1")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(data['paraphrases']), 2)
-        self.assertIn('The mat sat on the cat.', [p['tree'] for p in data['paraphrases']])
-        self.assertIn('The cat sat on the mat.', [p['tree'] for p in data['paraphrases']])
+        self.assertDictEqual(response.json, expected)
+
+    def test_paraphrase_endpoint_error(self):
+        expected = {"error": "Missing \"tree\" parameter."}
+        response = self.app.get("/paraphrase")
+        self.assertEqual(response.status_code, 400)
+        self.assertDictEqual(response.json, expected)
 
 if __name__ == '__main__':
     unittest.main()
